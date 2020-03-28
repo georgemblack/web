89c5092c323f4925c02f0166550fa76c8a2b06a5
georgemblack <contact@georgeblack.me>
2020-03-28 17:13:29 +0000

    Formatting

--- src/_scripts/analytics.js
+++ src/_scripts/analytics.js
@@ -1,5 +1,4 @@
 window.onload = function analytics() {
-
   if ('doNotTrack' in window.navigator && window.navigator.doNotTrack === '1') {
     console.log('Respecting \'do not track\' preferences')
     return
