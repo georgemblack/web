9258b3fd8cfcec5aa365e20a317f940a20baa928
georgemblack <contact@georgeblack.me>
2020-04-04 20:10:49 -0500

    Add GCS rsync action

---
+++ .github/actions/gcs-rsync/entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -e
+
+sanitize() {
+  if [ -z "${1}" ]
+  then
+    >&2 echo "Unable to find ${2}. Did you configure your workflow correctly?"
+    exit 1
+  fi
+}
+
+sanitize "${INPUT_PROJECTID}" "projectId"
+sanitize "${INPUT_BUCKETNAME}" "bucketName"
+sanitize "${GCLOUD_AUTH}" "GCLOUD_AUTH"
+
+# Set defaults
+PROJECT_ID=${INPUT_PROJECTID}
+BUCKET_NAME=${INPUT_BUCKETNAME}
+
+# Set project
+gcloud config set project ${PROJECT_ID}
+
+# Auth w/service account
+echo ${GCLOUD_AUTH} | base64 --decode > ./key.json
+gcloud auth activate-service-account --key-file=./key.json
+rm ./key.json
+
+# Upload dir
+gsutil -m rsync -r -d -x ".*assets/.*$" ./src/_site gs://${BUCKET_NAME}
