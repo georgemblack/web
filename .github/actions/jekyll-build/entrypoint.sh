5f46e0d8320056ba2b34f188970766bed25bb5ca
georgemblack <contact@georgeblack.me>
2020-04-08 20:10:08 -0500

    Update Jekyll build step

---
+++ .github/actions/jekyll-build/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -e
+
+sanitize() {
+  if [ -z "${1}" ]
+  then
+    >&2 echo "Unable to find ${2}. Did you configure your workflow correctly?"
+    exit 1
+  fi
+}
+
+sanitize "${INPUT_SOURCEDIRECTORY}" "directory"
+
+params=(
+  --config=_config.yml,_config_prod.yml
+)
+if [[ -z INPUT_SOURCEDIRECTORY ]]; then
+    params+=(-s ${INPUT_SOURCEDIRECTORY})
+fi
+if [[ -z INPUT_OUTPUTDIRECTORY ]]; then
+    params+=(-o ${INPUT_OUTPUTDIRECTORY)
+fi
+
+bundle install --gemfile=${INPUT_SOURCEDIRECTORY}/Gemfile
+bundle exec jekyll build "${params[@]}"
