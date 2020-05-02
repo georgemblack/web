cc3d88458cfbe1019696eec1a9a550ef6d4c99f6
georgemblack <contact@georgeblack.me>
2020-05-01 20:11:32 -0500

    Refactor worker script

--- worker/index.js
+++ worker/index.js
@@ -3,6 +3,7 @@ import mime from "mime";
 const EDGE_CACHE_TTL = "2592000";
 const ORIGIN = "https://georgeblack.me";
 const REMOTE_STORAGE_URL = "https://storage.googleapis.com/georgeblack.me";
+const NOT_FOUND_ASSET_PATHNAME = "404/index.html";
 
 addEventListener("fetch", (event) => {
   try {
@@ -16,87 +17,105 @@ addEventListener("fetch", (event) => {
  * Primary event handler
  */
 async function handleEvent(event) {
-  const cache = caches.default;
-  let response, pathKey;
-
-  // Check for valid method
   if (!["GET", "HEAD"].includes(event.request.method))
     return new Response("Bad request", { status: 400 });
 
-  const [pathname, mimeType] = getRequestInfo(event);
+  // route requests
+  const pathname = getPathname(event);
+  if (pathname.startsWith("assets/")) {
+    return await handleAssetFromURL(event);
+  }
+  return await handleAssetFromKV(event);
+}
 
-  // get key for this asset
-  // if an error is thrown, pathname is invalid
-  try {
-    pathKey = getPathKey(pathname);
-  } catch (e) {
+/**
+ * Fetch asset from Cloudflare Workers KV
+ */
+async function handleAssetFromKV(event) {
+  const cache = caches.default;
+  const pathname = getPathname(event);
+  const pathKey = getKVKey(pathname);
+
+  if (!pathKey) {
     return await getNotFoundResponse();
   }
 
-  // check cache for asset
-  // if cache hit, modify headers and return
-  response = await cache.match(`${ORIGIN}/${pathKey}`);
+  // check cache
+  let response = await cache.match(`${ORIGIN}/${pathKey}`);
   if (response) {
     let headers = new Headers(response.headers);
     response = new Response(response.body, { headers });
     response.headers.set("CF-Cache-Status", "HIT");
-    response.headers.set("Cache-Control", `public, max-age=${browserCacheMaxAge(pathKey)}`);
-    response.headers.delete("Age");
+    setBrowserHeaders(response, pathname);
     return response;
   }
 
-  // asset resides in Workers KV or Cloud Storage
-  try {
-    if (isRemoteAsset(pathKey)) {
-      response = await getAssetFromURL(pathKey, mimeType);
-    } else {
-      response = await getAssetFromKV(pathKey, mimeType);
-    }
-  } catch (e) {
-    return await getNotFoundResponse();
-  }
+  // build request
+  const mimeType = mime.getType(pathname) || "text/html";
+  const body = await __STATIC_CONTENT.get(pathKey, "arrayBuffer");
+  response = new Response(body, {
+    headers: { "Content-Type": mimeType },
+  });
 
-  // cache item
-  response.headers.set("Cache-Control", `max-age=${EDGE_CACHE_TTL}`);
+  // send to cache
+  setEdgeCacheHeaders(response);
   event.waitUntil(cache.put(`${ORIGIN}/${pathKey}`, response.clone()));
 
-  // set browser headers
-  response.headers.set("Cache-Control", `public, max-age=${browserCacheMaxAge(pathKey)}`);
+  setBrowserHeaders(response, pathname);
   response.headers.set("CF-Cache-Status", "MISS");
   return response;
 }
 
 /**
- * Retrieve asset from URL
+ * Fetch asset from a URL
  */
-async function getAssetFromURL(pathKey, mimeType) {
-  const response = await fetch(`${REMOTE_STORAGE_URL}/${pathKey}`);
-  if (response.status != 200) {
-    throw new Error("Asset not found in Cloud Storage!");
+async function handleAssetFromURL(event) {
+  const cache = caches.default;
+  const pathname = getPathname(event);
+
+  // check cache
+  let response = await cache.match(`${ORIGIN}/${pathname}`);
+  if (response) {
+    let headers = new Headers(response.headers);
+    response = new Response(response.body, { headers });
+    response.headers.set("CF-Cache-Status", "HIT");
+    setBrowserHeaders(response, pathname);
+    return response;
   }
-  return new Response(response.body, {
-    status: 200,
+
+  const remoteStorageResponse = await fetch(
+    `${REMOTE_STORAGE_URL}/${pathname}`
+  );
+  if (remoteStorageResponse.status != 200) {
+    return await getNotFoundResponse();
+  }
+
+  // build request
+  const mimeType = mime.getType(pathname) || "text/html";
+  response = new Response(remoteStorageResponse.body, {
     headers: { "Content-Type": mimeType },
   });
+
+  // send to cache
+  setEdgeCacheHeaders(response);
+  event.waitUntil(cache.put(`${ORIGIN}/${pathname}`, response.clone()));
+
+  // set browser headers
+  setBrowserHeaders(response, pathname);
+  response.headers.set("CF-Cache-Status", "MISS");
+  return response;
 }
 
 /**
- * Retrieve asset from Workers KV
- */
-async function getAssetFromKV(pathKey, mimeType) {
-  const body = await __STATIC_CONTENT.get(pathKey, "arrayBuffer");
-  return new Response(body, {
-    headers: { "Content-Type": mimeType },
-  });
-}
-
-/**
- * Return 404 page from KV
- * If 404 page does not exist, return text response
+ * Fetch 404 page from Cloudflare Workers KV
+ * If page doesn't exist, return text response
  */
 async function getNotFoundResponse() {
-  const pathKey = JSON.parse(__STATIC_CONTENT_MANIFEST)["404/index.html"];
-  if (!pathKey) return new Response("404 not found", { status: 404 });
+  const pathKey = getKVKey(NOT_FOUND_ASSET_PATHNAME);
+  if (!pathKey) {
+    return new Response("404 not found!", { status: 404 });
+  }
+
   const body = await __STATIC_CONTENT.get(pathKey, "arrayBuffer");
   return new Response(body, {
     status: 404,
@@ -105,53 +124,53 @@ async function getNotFoundResponse() {
 }
 
 /**
- * Calculate browser cache TTL
+ * Get key of object stored in Cloudflare Workers KV
  */
-function browserCacheMaxAge(pathname) {
-  const extension = pathname.split(".").pop();
-  if (/^(jpg|jpeg|png|webp|mov|ico|svg|webmanifest)$/.test(extension)) return "7776000"; // 90 days
-  if (/^(js|css)$/.test(extension)) return "86400"; // 1 day
-  return "900";
+function getKVKey(pathname) {
+  return JSON.parse(__STATIC_CONTENT_MANIFEST)[pathname];
 }
 
 /**
- * Get sanitized pathname and mime-type from event
+ * Get sanitized pathname of asset
  */
-function getRequestInfo(event) {
+function getPathname(event) {
   let pathname = new URL(event.request.url).pathname;
-  let mimeType;
-
-  // if path looks like dir, append index.html
   if (pathname.endsWith("/")) {
     pathname = pathname.concat("index.html");
-    mimeType = "text/html";
-  } else {
-    mimeType = mime.getType(pathname);
-    // if path doesn't look like valid content
-    if (!mimeType) {
-      pathname = pathname.concat("/index.html");
-      mimeType = "text/html";
-    }
   }
-  // remove leading slash
+  const filename = pathname.split("/").pop();
+  if (!filename.includes(".")) {
+    pathname = pathname.concat("/index.html");
+  }
   pathname = pathname.replace(/^\/+/, "");
-
-  return [pathname, mimeType];
-}
-
-/**
- * Get unique key for asset
- */
-function getPathKey(pathname) {
-  const pathKey = JSON.parse(__STATIC_CONTENT_MANIFEST)[pathname];
-  if (pathKey) return pathKey;
-  if (!isRemoteAsset(pathname)) throw new Error("Invalid path for asset!");
   return pathname;
 }
 
 /**
- * Return true if asset should reside in cloud storage
+ * Set headers for request to be sent to edge cache
  */
-function isRemoteAsset(pathname) {
-  return pathname.includes("assets/");
+function setEdgeCacheHeaders(response) {
+  response.headers.set("Cache-Control", `max-age=${EDGE_CACHE_TTL}`);
+}
+
+/**
+ * Set headers for request to be sent to browser
+ */
+function setBrowserHeaders(response, pathname) {
+  response.headers.delete("Age");
+  response.headers.set(
+    "Cache-Control",
+    `public, max-age=${browserCacheMaxAge(pathname)}`
+  );
+}
+
+/**
+ * Calculate the max age for an asset in the browser
+ */
+function browserCacheMaxAge(pathname) {
+  const extension = pathname.split(".").pop();
+  if (/^(jpg|jpeg|png|webp|mov|ico|svg|webmanifest)$/.test(extension))
+    return "7776000"; // 90 days
+  if (/^(js|css)$/.test(extension)) return "86400"; // 1 day
+  return "900";
 }
