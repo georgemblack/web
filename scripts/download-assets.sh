c9619d2989ef23484804c4f5c08dff0efe34a107
georgemblack <contact@georgeblack.me>
2020-05-10 12:54:40 -0500

    Remove unused scripts

--- scripts/download-assets.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-BUCKET_NAME="georgeblack.me"
-BUCKET_PREFIX="assets"
-SITE_BASE_DIR="."
-
-mkdir -p ${SITE_BASE_DIR}/src/assets
-gsutil -m rsync -r gs://${BUCKET_NAME}/${BUCKET_PREFIX} ${SITE_BASE_DIR}/src/assets
