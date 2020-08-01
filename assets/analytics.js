babc5b879cfb47a4819c9430bdf79e5750ca8c11
georgemblack <contact@georgeblack.me>
2020-07-31 20:21:17 -0500

    Static assets

---
+++ assets/analytics.js
@@ -0,0 +1,23 @@
+window.onload = function analytics() {
+  if ('doNotTrack' in window.navigator && window.navigator.doNotTrack === '1') {
+    console.log('Respecting \'do not track\' preferences')
+    return
+  }
+
+  const data = {
+    hostname: window.location.hostname,
+    pathname: window.location.pathname,
+    referrer: window.document.referrer,
+    windowInnerWidth: window.innerWidth,
+    timezone: Intl.DateTimeFormat().resolvedOptions().timeZone
+  }
+  
+  fetch('https://api.georgeblack.me/views', {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+}
