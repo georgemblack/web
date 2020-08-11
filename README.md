cbfd411376e0dd02abf2c458d24c87dcc811a7b4
georgemblack <contact@georgeblack.me>
2020-08-10 21:35:06 -0500

    Add basic API auth

--- README.md
+++ README.md
@@ -1,7 +1,29 @@
 # Web Builder
 
+Web Builder is a container that performs static site builds for [georgeblack.me](https://georgeblack.me). Its primary build function performs the following steps:
+
+1. Fetch data from API
+2. Use data to execute templates
+3. Upload static files to host
+
+When running locally, the server will listen on `9002`.
+
 Service requires following env vars:
 
+```
+PORT
+CLOUD_STORAGE_BUCKET
+```
+
+To authorize with build endpoint:
+
+```
+ADMIN_ORIGIN
+USERNAME
+PASSWORD
+```
+
+To authorize with data API:
 ```
 API_ENDPOINT
 API_USERNAME
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/base64"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/georgemblack/web"
 )
@@ -12,15 +14,55 @@ func main() {
 	port := getEnv("PORT", "9002")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		err := web.Build()
+		w.Header().Set("Access-Control-Allow-Origin", getEnv("ADMIN_ORIGIN", "*"))
+		w.Header().Set("Access-Control-Allow-Methods", "POST,OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Authorization")
+
+		if r.Method == "OPTIONS" {
+			return
+		}
+
+		if r.Method != "POST" {
+			log.Println("Method not allowed")
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		auth := strings.SplitN(r.Header.Get("Authorization"), " ", 2)
+		if len(auth) != 2 || auth[0] != "Basic" {
+			log.Println("Unauthorized access attempt")
+			http.Error(w, "Invalid authorization", http.StatusUnauthorized)
+			return
+		}
+
+		payload, err := base64.StdEncoding.DecodeString(auth[1])
 		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
+			log.Println("Invalid access attempt")
+			http.Error(w, "Invalid authorization", http.StatusBadRequest)
+			return
+		}
+
+		pair := strings.SplitN(string(payload), ":", 2)
+		if len(pair) != 2 || !validate(pair[0], pair[1]) {
+			log.Println("Unauthorized access attempt")
+			http.Error(w, "Authorization failed", http.StatusUnauthorized)
+			return
+		}
+
+		err = web.Build()
+		if err != nil {
 			log.Println(err)
+			http.Error(w, "Build failed", http.StatusInternalServerError)
+			return
 		}
 	})
 
 	log.Println("Listening on " + port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+func validate(username, password string) bool {
+	return username == getEnv("USERNAME", "test") && password == getEnv("PASSWORD", "test")
 }
 
 func getEnv(key, fallback string) string {
