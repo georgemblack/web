d3373e21a2feaa83adbab8445461be40528fa6d3
georgemblack <contact@georgeblack.me>
2020-09-13 15:59:06 -0500

    Minor refactor

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ const (
 )
 
 var outputDirectory string
+var siteContent SiteContent
 
 // Build starts build process
 func Build() (string, error) {
@@ -30,83 +31,47 @@ func Build() (string, error) {
 		return "", err
 	}
 	log.Println("Found " + strconv.Itoa(len(posts.Posts)) + " post(s)")
-
 	likes, err := getAllLikes()
 	if err != nil {
 		return "", err
 	}
 	log.Println("Found " + strconv.Itoa(len(likes.Likes)) + " likes(s)")
-
 	posts, err = processPostsContent(posts)
 	if err != nil {
 		return "", err
 	}
 	log.Println("Processing content for " + strconv.Itoa(len(likes.Likes)) + " post(s)")
 
-	siteMetadata := getDefaultSiteMetadata()
-	siteContent := SiteContent{posts, likes}
+	siteContent = SiteContent{posts, likes}
 
-	// build index page
-	builder := Builder{}
-	builder.SiteMetadata = siteMetadata
-	builder.SiteContent = siteContent
-	builder.Data = make(map[string]interface{})
-
-	if err := buildIndexPage(builder); err != nil {
+	// begin build steps
+	if err := buildIndexPage(newBuilder()); err != nil {
 		log.Println("Error while building index page")
 		return "", err
 	}
-
-	// build standard pages
-	builder = Builder{}
-	builder.SiteMetadata = siteMetadata
-	builder.SiteContent = siteContent
-	builder.Data = make(map[string]interface{})
-
-	if err := buildStandardPages(builder); err != nil {
+	if err := buildStandardPages(newBuilder()); err != nil {
 		log.Println("Error while building standard pages")
 		return "", err
 	}
-
-	// build feeds
-	builder = Builder{}
-	builder.SiteMetadata = siteMetadata
-	builder.SiteContent = siteContent
-	builder.Data = make(map[string]interface{})
-
-	if err := buildFeeds(builder); err != nil {
+	if err := buildFeeds(newBuilder()); err != nil {
 		log.Println("Error while building feeds")
 		return "", err
 	}
-
-	// build sitemap
-	builder = Builder{}
-	builder.SiteMetadata = siteMetadata
-	builder.SiteContent = siteContent
-	builder.Data = make(map[string]interface{})
-
-	if err := buildSitemap(builder); err != nil {
+	if err := buildSitemap(newBuilder()); err != nil {
 		log.Println("Error while building sitemap")
 		return "", err
 	}
-
-	// build post pages
-	builder = Builder{}
-	builder.SiteMetadata = siteMetadata
-	builder.SiteContent = siteContent
-	builder.Data = make(map[string]interface{})
-
-	if err := buildPostPages(builder); err != nil {
+	if err := buildPostPages(newBuilder()); err != nil {
 		log.Println("Error while building post pages")
 		return "", err
 	}
 
 	log.Println("Copying static files to destination...")
-	filePaths, err := staticSiteFiles()
+	paths, err := staticSiteFiles()
 	if err != nil {
 		return "", err
 	}
-	for _, path := range filePaths {
+	for _, path := range paths {
 		destPath := strings.Replace(path, "site", outputDirectory, 1)
 		split := strings.Split(destPath, "/")
 		destDir := strings.Join(split[:len(split)-1], "/")
@@ -143,4 +108,12 @@ func Publish(buildID string) error {
 
 	log.Println("Completed publish for build: " + buildID)
 	return nil
+}
+
+func newBuilder() Builder {
+	builder := Builder{}
+	builder.SiteMetadata = getDefaultSiteMetadata()
+	builder.SiteContent = siteContent
+	builder.Data = make(map[string]interface{})
+	return builder
 }
