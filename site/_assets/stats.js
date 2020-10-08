64beed7f764c8319fd683447a9e1ef0058cfaf86
georgemblack <contact@george.black>
2020-10-08 20:37:16 +0000

    Inline and minify scripts

---
+++ site/_assets/stats.js
@@ -0,0 +1,21 @@
+window.onload = function analytics() {
+  if ('doNotTrack' in window.navigator && window.navigator.doNotTrack === '1') {
+    console.log('Respecting \'do not track\' preferences')
+    return
+  }
+  const data = {
+    hostname: window.location.hostname,
+    pathname: window.location.pathname,
+    referrer: window.document.referrer,
+    windowInnerWidth: window.innerWidth,
+    timezone: Intl.DateTimeFormat().resolvedOptions().timeZone
+  }
+  fetch('https://views.george.black', {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+}
