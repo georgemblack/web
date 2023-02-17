8698e64ff99bd7c595d9936dbfacf0cc50a0e741
George Black <contact@george.black>
2023-02-17 19:50:17 +0000

    One more Codespaces test

---
+++ .devcontainer/startup.sh
@@ -0,0 +1,3 @@
+export GOOGLE_APPLICATION_CREDENTIALS=google-application-credentials.json
+export ENVIRONMENT=staging
+echo "$GOOGLE_APPLICATION_CREDENTIALS_CONTENTS" > google-application-credentials.json
