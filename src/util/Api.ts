dea55e5c3665f1d19b16963d897c2fc909adf605
George Black <contact@george.black>
2024-10-06 20:29:57 -0500

    Migrate to Kirby headless CMS

--- src/util/Api.ts
+++ src/util/Api.ts
@@ -1,69 +1,38 @@
 const API_URL = import.meta.env.API_URL;
-const API_TOKEN = import.meta.env.API_TOKEN;
-
-export interface AuthResponse {
-  token: string;
-}
-
-export interface PostsResponse {
-  posts: Post[];
-}
 
 export interface Post {
   title: string;
   slug: string;
   published: string;
-  listed: boolean;
-  contentHtml: string;
-  contentHtmlPreview?: string;
-}
-
-export interface LikeResponse {
-  likes: Like[];
+  visible: boolean;
+  preview: string;
+  content: string;
 }
 
 export interface Like {
   title: string;
   url: string;
-  timestamp: string;
+  published: string;
 }
 
 export async function getPosts(): Promise<Post[]> {
-  const authResponse = await fetch(`${API_URL}/auth`, {
-    method: "POST",
+  const response = await fetch(`${API_URL}/blog.json`, {
+    method: "GET",
     headers: {
       "Content-Type": "application/json",
-      Authorization: "Bearer " + API_TOKEN,
     },
   });
-  const auth: AuthResponse = await authResponse.json();
 
-  const postsResponse = await fetch(`${API_URL}/posts?published=true`, {
-    headers: {
-      Authorization: "Bearer " + auth.token,
-    },
-  });
-  const posts: PostsResponse = await postsResponse.json();
-
-  return posts.posts;
+  return await response.json();
 }
 
 export async function getLikes(): Promise<Like[]> {
-  const authResponse = await fetch(`${API_URL}/auth`, {
-    method: "POST",
+  const response = await fetch(`${API_URL}/likes.json`, {
+    method: "GET",
     headers: {
       "Content-Type": "application/json",
-      Authorization: "Bearer " + API_TOKEN,
     },
   });
-  const auth: AuthResponse = await authResponse.json();
 
-  const likesResponse = await fetch(`${API_URL}/likes`, {
-    headers: {
-      Authorization: "Bearer " + auth.token,
-    },
-  });
-  const likes: LikeResponse = await likesResponse.json();
-
-  return likes.likes;
+  return await response.json();
 }
