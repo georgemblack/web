8e5ebe2e164bf8dbc5bbf159238f8d44a3cc1d42
George Black <contact@george.black>
2024-10-10 11:23:44 -0500

    Fix API interface & add support for previews

--- src/util/Api.ts
+++ src/util/Api.ts
@@ -1,6 +1,7 @@
 const API_URL = import.meta.env.API_URL;
 
 export interface Post {
+  id: string;
   title: string;
   slug: string;
   published: string;
@@ -10,9 +11,12 @@ export interface Post {
 }
 
 export interface Like {
+  id: string;
   title: string;
-  url: string;
+  slug: string;
   published: string;
+  url: string;
+  content: string;
 }
 
 export async function getPosts(): Promise<Post[]> {
@@ -23,11 +27,20 @@ export async function getPosts(): Promise<Post[]> {
     },
   });
 
-  return await response.json();
+  const posts = await response.json();
+
+  // For each post, add a preview field.
+  // If the post content has a '<div class="break"></div>' element, use the content up to (but not including) that element.
+  // If there is no 'break', use the full content as the preview.
+  return posts.map((post: Post) => {
+    const index = post.content.indexOf('<div class="break"></div>');
+    post.preview = index === -1 ? post.content : post.content.slice(0, index);
+    return post;
+  });
 }
 
 export async function getLikes(): Promise<Like[]> {
-  const response = await fetch(`${API_URL}/likes.json`, {
+  const response = await fetch(`${API_URL}/links.json`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
