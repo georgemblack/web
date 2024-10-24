246015d30afd7428a85628e9177a01da1ea25511
George Black <contact@george.black>
2024-10-24 06:08:00 -0500

    Include link posts in XML feed

--- src/pages/feeds/main.xml.js
+++ src/pages/feeds/main.xml.js
@@ -1,20 +1,20 @@
 import rss from "@astrojs/rss";
-import { getPosts } from "../../util/Api";
-import { slug } from "../../util/Format";
+import { getCombined, getPosts } from "../../util/Api";
+import { url } from "../../util/Format";
 
 export async function GET(context) {
-  const posts = await getPosts();
+  const combined = await getCombined();
   return rss({
     title: "George Black",
     description:
       "George is a software engineer working in Austin, with a small home on the internet.",
     site: context.site,
-    items: posts.map((post) => ({
-      title: post.title,
-      pubDate: post.published,
-      link: `/${slug(post)}`,
+    items: combined.map((item) => ({
+      title: item.title,
+      pubDate: item.published,
+      link: url(item),
       trailingSlash: false,
-      content: post.content,
+      content: item.content,
     })),
   });
 }
