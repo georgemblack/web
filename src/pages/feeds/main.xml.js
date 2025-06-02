2255052812846c481feeb48b9e6358a92d95f31e
George Black <contact@george.black>
2025-06-01 21:33:32 -0500

    Fix remaining references to like items

--- src/pages/feeds/main.xml.js
+++ src/pages/feeds/main.xml.js
@@ -1,15 +1,15 @@
 import rss from "@astrojs/rss";
-import { getCombined } from "../../util/Api";
+import { getPosts } from "../../util/Api";
 import { url } from "../../util/Format";
 
 export async function GET(context) {
-  const combined = await getCombined();
+  const posts = await getPosts();
   return rss({
     title: "George Black",
     description:
       "George is a software engineer working in Austin, with a small home on the internet.",
     site: context.site,
-    items: combined.map((item) => ({
+    items: posts.map((item) => ({
       title: item.title,
       pubDate: item.published,
       link: url(item),
