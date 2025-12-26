f199a4da6d1b39f758b6df790c221d9e0e377255
George Black <gblack@cloudflare.com>
2025-12-26 13:43:03 -0600

    Process/optimize images

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,40 +1,42 @@
+const CACHE_CONTROL = "public, max-age=31536000";
+
 export default {
   async fetch(request, env, ctx): Promise<Response> {
     const url = new URL(request.url);
-    const key = url.pathname.slice(1);
+    const key = url.pathname.replace("/files/", "");
 
-    // 1. Check cache
     const cache = caches.default;
     const cachedResponse = await cache.match(request);
-    if (cachedResponse) {
-      return cachedResponse;
-    }
+    if (cachedResponse) return cachedResponse;
 
-    // 2. Check R2 bucket for the object
     const object = await env.WEB_FILES.get(key);
-    if (!object) {
-      return new Response("Not Found", { status: 404 });
-    }
+    if (!object) return new Response("Not Found", { status: 404 });
 
-    // 3. Read optimized_files from KV
-    const optimizedFiles = await env.WEB_FILES_META.get<string[]>(
+    const toOptimize = await env.WEB_FILES_META.get<string[]>(
       "optimized_files",
       "json"
     );
-    // TODO: Use optimizedFiles to determine if file should be optimized
 
-    // 4. Build response and cache it
+    if (toOptimize && toOptimize.includes(key)) {
+      const optimized = await env.IMAGES.input(object.body)
+        .transform({ width: 900 })
+        .output({ format: "image/avif" });
+
+      const response = optimized.response();
+      response.headers.set("cache-control", CACHE_CONTROL);
+      ctx.waitUntil(cache.put(request, response.clone()));
+      return response;
+    }
+
     const headers = new Headers();
     object.writeHttpMetadata(headers);
     headers.set("etag", object.httpEtag);
-    headers.set("cache-control", "public, max-age=31536000");
+    headers.set("cache-control", CACHE_CONTROL);
 
     const response = new Response(object.body, {
       headers,
     });
-
     ctx.waitUntil(cache.put(request, response.clone()));
-
     return response;
   },
 } satisfies ExportedHandler<Env>;
